ec8a4b3ac79927a884096a6df38bd70a9ae155c6
Robby van Rheenen <robbyvanrheenen@gmail.com>
2018-11-01 23:33:42 +0100

    create Area Constructor

--- area.js
+++ area.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function Area(x, y, width, heigth) {
+  this.backgroundImageUrl = '';
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.heigth = heigth;
+}
+
+Area.prototype.setBackgroundImage = function(url) {
+  this.backgroundImageUrl = url;
+}
