ff9bec67cf4996f475ba9ce7615349f5ba39ce92
Robby van Rheenen <robbyvanrheenen@gmail.com>
2018-11-01 19:45:56 +0100

    create empty Main Constructor

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function main() {
+  //console.log('test if eventListener is working');
+
+  function buildSplash() {
+
+  }
+
+  function destroySplash() {
+
+  }
+
+  function buildGame() {
+
+  }
+
+  function destroyGame() {
+
+  }
+
+  function buildGameOver() {
+
+  }
+
+  function destroyGameOver() {
+
+  }
+}
+
+document.addEventListener('load', main());
